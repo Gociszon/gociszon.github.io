7514cda930d7f4c9c8c3a53997ad01de83b092f6
Gociszon <gociszon@gmail.com>
2024-12-05 14:57:20 +0100

    feat: marking products as purchased

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
 function EnterProduct() {
   const [productList, setProductList] = useState([]);
   const [product, setProduct] = useState("");
+  const [style, setStyle] = useState("product");
 
   const addProduct = () => {
     setProductList(productList => [...productList, [product]]);
@@ -22,13 +23,26 @@ function EnterProduct() {
     setProduct(event.target.value);
   }
 
+  const deleteProduct = () => {
+
+  }
+
+  const changePurchased = () => {
+    if (style !== 'purchased') setStyle("purchased");
+    else setStyle("product");
+  }
+
   return (
     <>
       <input type='text' value={product} onChange={changeProduct}/>
       <button onClick={addProduct}>Add</button>
 
       <ul>
-        {productList.map((product, index) => <li key={index}>{product}</li>)}
+        {productList.map((product, index) => <li key={index}>
+          <p><span onClick={changePurchased} className={style}>{product}</span>
+          <button style={{backgroundColor: "red", width: "25px", height: "25px"}} onClick={deleteProduct}></button>
+          </p>
+          </li>)}
       </ul>
     </>
   );
